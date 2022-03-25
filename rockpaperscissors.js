2bf78d94df093adfe72b9afdeda27647adbbe9c5
Sammy <sammylam_1@hotmail.com>
2022-03-24 22:13:59 -0400

    first add

---
+++ rockpaperscissors.js
@@ -0,0 +1,45 @@
+let playerScore = 0
+let computerScore = 0
+let roundWinner = ''
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    roundWinner = 'tie'
+  }
+  if (
+    (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
+    (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') ||
+    (playerSelection === 'PAPER' && computerSelection === 'ROCK')
+  ) {
+    playerScore++
+    roundWinner = 'You have won this day of fighting. Congratulations.'
+  }
+  if (
+    (computerSelection === 'ROCK' && playerSelection === 'SCISSORS') ||
+    (computerSelection === 'SCISSORS' && playerSelection === 'PAPER') ||
+    (computerSelection === 'PAPER' && playerSelection === 'ROCK')
+  ) {
+    computerScore++
+    roundWinner = 'You have been defeated. The day is over. Try again tommorow.'
+  }
+  updateScoreMessage(roundWinner, playerSelection, computerSelection)
+}
+
+
+function getRandomChoice() {
+  let randomNumber = Math.floor(Math.random() * 3)
+  switch (randomNumber) {
+    case 0:
+      return 'ROCK'
+    case 1:
+      return 'PAPER'
+    case 2:
+      return 'SCISSORS'
+  }
+}
+
+function isGameOver(){
+    return playerScore == 5 || computerScore == 5
+}
+
+
