47c982553af29af2daa04ed5d9f84fa0724a36a3
Sammy <sammylam_1@hotmail.com>
2022-03-29 21:24:17 -0400

    working js

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,45 +1,59 @@
-let playerScore = 0
-let computerScore = 0
-let roundWinner = ''
+const selectionButtons = document.querySelectorAll('[data-selection]');
+const computerScoreSpan = document.querySelector("[data-computer-score]")
+const yourScoreSpan = document.querySelector("[data-your-score]")
+const endDesc = document.querySelector("endDesc")
+const SELECTIONS = [
+  {
+    name: 'rock',
+    beats: 'scissors'
+  },
+  {
+    name: 'paper',
+    beats: 'rock'
+  },
+  {
+    name: 'scissors',
+    beats: 'paper'
+  }
+]
 
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    roundWinner = 'tie'
+
+selectionButtons.forEach(selectionButton => {
+  selectionButton.addEventListener('click', e => {
+    const selectionName = selectionButton.dataset.selection
+    const selection = SELECTIONS.find(selection => selection.name === selectionName)
+    makeSelection(selection)
+  })
+})
+
+function makeSelection(selection){
+  const computerSelection = randomSelection()
+  const yourWinner = IsWinner(selection, computerSelection)
+  const computerWinner = IsWinner(computerSelection, selection)
+  if (yourWinner) incrementScore(yourScoreSpan)
+  if (computerWinner) incrementScore(computerScoreSpan)
+  if (computerScoreSpan === 5 | yourScoreSpan === 5) {
+    declareWinner();
   }
-  if (
-    (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
-    (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') ||
-    (playerSelection === 'PAPER' && computerSelection === 'ROCK')
-  ) {
-    playerScore++
-    roundWinner = 'You have won this day of fighting. Congratulations.'
-  }
-  if (
-    (computerSelection === 'ROCK' && playerSelection === 'SCISSORS') ||
-    (computerSelection === 'SCISSORS' && playerSelection === 'PAPER') ||
-    (computerSelection === 'PAPER' && playerSelection === 'ROCK')
-  ) {
-    computerScore++
-    roundWinner = 'You have been defeated. The day is over. Try again tommorow.'
-  }
-  updateScoreMessage(roundWinner, playerSelection, computerSelection)
+}
+
+function incrementScore(scoreSpan){
+  scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
+}
+
+function declareWinner(){
+  endDesc = 'you have won'
+}
+
+function IsWinner(selection, opponentSelection){
+  return selection.beats === opponentSelection.name
+}
+
+function randomSelection(){
+  const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
+  return SELECTIONS[randomIndex]
 }
 
 
-function getRandomChoice() {
-  let randomNumber = Math.floor(Math.random() * 3)
-  switch (randomNumber) {
-    case 0:
-      return 'ROCK'
-    case 1:
-      return 'PAPER'
-    case 2:
-      return 'SCISSORS'
-  }
-}
 
-function isGameOver(){
-    return playerScore == 5 || computerScore == 5
-}
-
-
+    
